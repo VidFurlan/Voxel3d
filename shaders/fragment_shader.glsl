01f79bec7d666ddde1bc424f450c2fe3e985383e
Vid Furlan <nalrufdiv@gmail.com>
2024-01-07 18:18:01 +0100

    3d cube rendering

--- shaders/fragment_shader.glsl
+++ shaders/fragment_shader.glsl
@@ -1,8 +1,10 @@
 #version 330 core
 
+in vec3 fragmentColor;
+
 out vec3 color;
 
 void main()
 {
-	color = vec3(0.0,0.7,0.9);
+    color = fragmentColor;
 }
--- shaders/vertex_shader.glsl
+++ shaders/vertex_shader.glsl
@@ -1,9 +1,14 @@
 #version 330 core
 
-// Input vertex data, different for all executions of this shader.
 layout(location = 0) in vec3 vertexPosition_modelspace;
+layout(location = 1) in vec3 vertexColor;
 
-void main(){
-    gl_Position.xyz = vertexPosition_modelspace;
-    gl_Position.w = 1.0;
+out vec3 fragmentColor;
+uniform mat4 MVP;
+
+void main(){	
+
+	gl_Position =  MVP * vec4(vertexPosition_modelspace,1);
+
+	fragmentColor = vertexColor;
 }
